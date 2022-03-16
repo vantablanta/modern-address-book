a59d30e049b3503f9930e0ca3752eb85c8ef51f4
vanta blanta <vantablanta@gmail.com>
2022-03-16 14:54:45 +0300

    on click & mouse out event added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 //business- logic 
-function ContactDetails(name, email, address1, address2, city, state, zip){
+function ContactDetails(name, email, address1, address2, city, state, zip) {
     this.name = name
     this.email = email
     this.address1 = address1
@@ -11,31 +11,35 @@ function ContactDetails(name, email, address1, address2, city, state, zip){
 }
 
 //ui
-$(document).ready(function(){
-    $("#form").submit(function(e){
+$(document).ready(function () {
+    $("#form").submit(function (e) {
         e.preventDefault();
         let contactName = $("#inputName4").val()
         let contactEmail = $("#inputEmail4").val()
         let contactAddress = $("#inputAddress").val()
         let contactAddress2 = $("#inputAddress2").val()
-        let contactCity  = $("#nputCity").val()
+        let contactCity = $("#nputCity").val()
         let contactState = $("#inputState").val()
         let contactZip = $("#inputZip").val()
-        let person = new ContactDetails(contactName, contactEmail, contactAddress, contactAddress2, contactCity, contactState, contactZip )
-       
-  
-     
-                $("#display").append(`<p> ${person.name} </p>`)
-                $("p").addClass("btn btn-secondary d-flex flex-column")
-                form.reset()
-                $("p").click(function () {
-                    $("#contactDetails").append(`<p> Email: ${contactEmail}`)
-                })
-        
-
-    })   
+        let person = new ContactDetails(contactName, contactEmail, contactAddress, contactAddress2, contactCity, contactState, contactZip)
+        $("#display").append(`<p> ${person.name} </p>`)
+        $("p").addClass("btn btn-secondary d-flex flex-column")
+        form.reset()
+        $("p").click(function () {
+            $("#contactDetails").append(`
+                        <p> ${contactAddress}</p>
+                        <p> ${contactAddress2}</p>
+                        <p> ${contactCity}</p>
+                        <p> ${contactState}</p>
+                        <p> ${contactZip}</p>`
+            )
+        })
+        $("p").mouseout(function () {
+            $("#contactDetails").empty()
+        })
+    })
 })
-   
+
 
 
 
